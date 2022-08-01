574ef5ad2db03e0587af88edafb44c84d1d031b8
Aleksandr <aleksandrfomkin@outlook.com>
2022-08-01 22:18:55 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
+
+//        MyThread thread1 = new MyThread(1);
+//        MyThread thread2 = new MyThread(2);
+//        thread1.start();
+//        thread2.start(); // пример работы многопоточности
+
         String folderPath = "C:\\Users\\Aleksandr.DESKTOP-ETBP0DQ\\Desktop\\Программы";
         File file = new File(folderPath);
-        System.out.println(getFolderSize(file));
+
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
     }
     public static long getFolderSize(File folder){
         if(folder.isFile()){
